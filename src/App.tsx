7b434f30f4d78a18509988619ec798aa8e7719a3
chienlx <xuanchiena3@gmail.com>
2022-12-07 15:00:59 +0700

    test tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './App.css';
 
+let name:string = "chienlx";
+let age:number = 20;
+let isCheck:boolean = true;
+let projectString:string[] = ['text','text1']
+projectString[0] = "20"
+
+type Student = {
+  name: string,
+  age : 20
+}
+
 function App() {
   return (
     <div className="App">
